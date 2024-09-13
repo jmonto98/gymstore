f6fc79ecab6013d0d70630ee10b2af784f436fd9
dposada13 <dposada13@gmail.com>
2024-09-13 18:35:19 -0500

    Create user view

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,12 +15,17 @@
               <input type="email" pattern=".+@example\.com" size="30" class="form-control mb-2" placeholder="Enter Email" name="email" require/>
               <input type="text" class="form-control mb-2" placeholder="Enter Username" name="username" require/>
               <input type="password" minlength="8" class="form-control mb-2" placeholder="Enter Password" name="password" require/>
-              <select class="form-control" name="species" id="species" required placeholder="Enter Rol"> 
+              <select class="form-control" name="Rol" id="species" required placeholder="Enter Rol"> 
                 <option value="">Select Role</option> 
                 <option value="0">Administrator</option> 
                 <option value="1">User</option> 
               </select><br>
-              <input type="text" class="form-control mb-2" placeholder="State" name="state" require/>
+             <!--  <input type="text" class="form-control mb-2" placeholder="State" name="state" require/> -->
+             <select class="form-control" name="state" id="species" required placeholder="Enter state"> 
+                <option value="">Select State</option> 
+                <option value="0">Active</option> 
+                <option value="1">Inactive</option> 
+              </select><br>
               <input type="text" class="form-control mb-2" placeholder="Enter Balance" name="balance" require/>
               
               <input type="submit" class="btn btn-primary" value="Save" />
