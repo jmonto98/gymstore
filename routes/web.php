bc23343a033d79ee485172ce535bd115cbe0521d
jmontoya <jmontoya@celerik.com>
2024-09-13 23:54:14 -0500

    Login Implemented

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.index');
+
+Auth::routes();
