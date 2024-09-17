e946f48560e4aff6045cb54345e86d22bbeffd2d
Orleis Quiceno <oquicen2@gmail.com>
2024-09-17 14:44:21 -0500

    Review

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+    public function store(Request $request, $productId)
+    {
+        $request->validate([
+            'rating' => 'required|integer|min:1|max:5',
+            'comment' => 'required|string',
+        ]);
+
+        Review::create([
+            'user_id' => Auth::id(),
+            'product_id' => $productId, 
+            'rating' => $request->input('rating'),
+            'comment' => $request->input('comment'),
+            'approved' => false, 
+        ]);
+
+        return redirect()->back()->with('success', 'Review submitted successfully!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('
 Route::middleware('auth')->group(function () {
     Route::get('/cart/purchase', 'App\Http\Controllers\CartController@purchase')->name('cart.purchase');
     Route::get('/my-account/orders', 'App\Http\Controllers\MyAccountController@orders')->name('myaccount.orders');
+
+    Route::post('/products/{id}/reviews', 'App\Http\Controllers\ReviewController@store')->name('review.store');
 });
 
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name('admin.home.index');
