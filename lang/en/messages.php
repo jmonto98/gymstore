21c707faab6a88778483fc1ed9080697c20d4b60
jmontoya <jmontoya@celerik.com>
2024-09-19 23:43:23 -0500

    Lang to app.blade

---
+++ lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'brand' => 'GymStore',
+    'home' => 'Home',
+    'products' => 'Products',
+    'about' => 'About',
+    'contact_us' => 'Contact Us',
+    'login' => 'Login',
+    'register' => 'Register',
+    'cart' => 'Cart',
+    'logout' => 'Logout',
+];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,26 +14,26 @@
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary py-4">
         <div class="container">
-            <a class="navbar-brand" href="/">GymStore</a>
+            <a class="navbar-brand" href="/">{{ __('messages.brand') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
-                    <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
-                    <a class="nav-link active" href="">About</a>
-                    <a class="nav-link active" href="">Contact Us</a>
+                    <a class="nav-link active" href="{{ route('home.index') }}">{{ __('messages.home') }}</a>
+                    <a class="nav-link active" href="{{ route('product.index') }}">{{ __('messages.products') }}</a>
+                    <a class="nav-link active" href="">{{ __('messages.about') }}</a>
+                    <a class="nav-link active" href="">{{ __('messages.contact_us') }}</a>
                     <div class="vr bg-white mx-2 d-none d-lg-block"></div>
                     @guest
-                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
-                    <a class="nav-link active" href="{{ route('register') }}">Register</a>
-                    <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
+                    <a class="nav-link active" href="{{ route('login') }}">{{ __('messages.login') }}</a>
+                    <a class="nav-link active" href="{{ route('register') }}">{{ __('messages.register') }}</a>
+                    <a class="nav-link active" href="{{ route('cart.index') }}">{{ __('messages.cart') }}</a>
                     @else
                     <form id="logout" action="{{ route('logout') }}" method="POST">
-                        <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
-                        <a role="button" class="nav-link active" onclick="document.getElementById('logout').submit();">Logout</a>
+                        <a class="nav-link active" href="{{ route('cart.index') }}">{{ __('messages.cart') }}</a>
+                        <a role="button" class="nav-link active" onclick="document.getElementById('logout').submit();">{{ __('messages.logout') }}</a>
                         @csrf
                     </form>
                     @endguest
