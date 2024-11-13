64446514e9ac29adfd7fd7a542cbb42be455a9e5
dposada13 <dposada13@gmail.com>
2024-11-12 20:41:49 -0500

    Change Show Order

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Item;
 use App\Models\Order;
-use App\Models\Product;
 use Illuminate\View\View;
 
 class AdminOrderController extends Controller
@@ -25,30 +24,15 @@ class AdminOrderController extends Controller
         $viewData = [];
         $viewData['title'] = __('messages.order');
         $viewData['subtitle'] = __('messages.show_order');
-        $order= Order::with(['items.product']) -> where ('id', $id)-> first();
+        $order = Order::where('id', $id)->get();
+        $items = Item::where('order_id', $id)->get();
 
-        if (!$order) {
+        if (! $order) {
             return redirect()->back()->with('error', 'Order not found');
         }
 
-        $orderDetails = $order->items->map(function ($item) {
-            return [
-                'product_name' => $item->product->name,
-                'quantity' => $item->quantity,
-                'price' => $item->price,
-            ];
-        });
-
-        $viewData ['orderDetails']= $orderDetails;
-        $viewData ['order']=$order;
-
-        
-
-
-
-        // $viewData['orders'] = Order::findOrFail($id)->get();
-        // $viewData['product'] = Product::all();
-        // $viewData['item'] = Item::all();
+        $viewData['order'] = $order;
+        $viewData['items'] = $items;
 
         return view('admin.order.show')->with('viewData', $viewData);
     }
--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -9,7 +9,9 @@
     @endif
     <div class='card mb-4'>
         <div class='card-header bg-primary text-white'>
-            <h4 class='mb-0'>{{ __('messages.show_details') }}</h4>
+        @foreach ($viewData['order'] as $orders)
+            <h4 class='mb-0'>{{ __('messages.show_details') }} {{ $orders->getId()  }}</h4>
+        @endforeach  
         </div>
     </div>
     <div class='card mt-4'>
@@ -24,19 +26,27 @@
             @endif
                 <thead>
                     <tr>
-                        <th scope='col'>{{ __('messages.order_id') }}</th>
                         <th scope='col'>{{ __('messages.product') }}</th>
                         <th scope='col'>{{ __('messages.quantity') }}</th>
-                        <th scope='col'>{{ __('messages.price') }}</th>                  
+                        <th scope='col'>{{ __('messages.unit_price') }}</th> 
+                        <th scope='col'>{{ __('messages.total_items') }}</th> 
                     </tr>
                 </thead>
                 <tbody>
-                @foreach ($viewData['order'] as $detail)
+                @foreach ($viewData['items'] as $item)
                     <tr>
-                    <td></td>
+                        <td>{{ $item->product->getName() }}</td>
+                        <td>{{ $item->getQuantity() }}</td>
+                        <td>{{ $item->getPrice() }}</td>
+                        <td>{{ $item->getPrice() *  $item->getQuantity() }}</td>
                     </tr>
                 @endforeach
                 </tbody>
+
+            <tr>
+                <td colspan="3"><b>{{ __('messages.total_order') }}</td></b>
+                <td ><b>{{ $item->order->getTotalOrder() }}</td></b>
+            </tr>
             </table>
             <td>
                 <a class='btn btn-primary' href='{{ route('admin.order.index')}}'>
