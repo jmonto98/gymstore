9c40c9c7d2ab67a8c0c9a15cc7d58ea1fcc84a98
jmpalaci <oquicen2@gmail.com>
2024-11-18 23:30:42 -0500

    Buy product

--- app/Http/Controllers/Partner/PartnerController.php
+++ app/Http/Controllers/Partner/PartnerController.php
@@ -11,12 +11,9 @@ class PartnerController extends Controller
         $viewData = [];
         $viewData['title'] = __('messages.partner_title');
         $viewData['subtitle'] = __('messages.partner_subtitle');
-        
         $response = Http::get('http://34.71.100.126/public/api/products'); 
         $partners = $response->json(); 
-
         $viewData['partners'] = $partners; 
-
         return view('partner.index')->with('viewData', $viewData);
     }
 }
--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -6,11 +6,14 @@
   @foreach ($viewData['partners'] as $partner)
     <div class="col-md-4 col-lg-3 mb-2">
       <div class="card">
-        <img src="{{ $partner['url'] }}" class="card-img-top" alt="{{ $partner['name'] }}" width="300" height="300">
+        <img src="{{ $partner['image'] }}" class="card-img-top" alt="{{ $partner['name'] }}" width="300" height="300">
         <div class="card-body text-center">
           <h5 class="card-title">{{ $partner['name'] }}</h5>
           <p class="card-text">{{ $partner['description'] }}</p>
           <p class="card-text"><strong>Precio:</strong> ${{ number_format($partner['price'], 2) }}</p>
+          <a href="{{ $partner['url'] }}" target="_blank" class="btn btn-primary">
+            Buy product
+          </a>
         </div>
       </div>
     </div>
